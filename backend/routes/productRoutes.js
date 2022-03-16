83b1b6138b438c32fbe6066f7b5fc353fe250893
MahiPalihawadana <dilkipalihawadana95@gmail.com>
2022-03-16 19:09:28 +0530

    backend initial setup done - one route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,3 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getProducts,
+  getProductById,
+} = require("../controller/productControllers");
 
-const express = rquire('express');
-const router=express.Router();
+
+//@dec GET sll products from db
+//@route GET /api/products
+//@access Public
+router.get("/", getProducts);
+
+//@desc GET a product by id from db
+//@route GET /api/products/:id
+//@access Public
+router.get("/:id", getProductById);
+
+module.exports = router;
