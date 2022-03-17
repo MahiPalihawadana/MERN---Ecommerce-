50ce75f93bdf4321d68ae20788f77d228de2f388
MahiPalihawadana <dilkipalihawadana95@gmail.com>
2022-03-17 13:04:10 +0530

    Backdrop - done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screens/CartScreen';
 
 //components
 import Navbar from './components/Navbar';
+import Backdrop from './components/Backdrop';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
 
       <Navbar />
       { /* SideDrawer */}
-      { /* Backdrop */}
+      <Backdrop/>
+      
 
       <main>
         <Routes>
