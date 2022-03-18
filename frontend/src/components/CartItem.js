4d3030e6e5d20a5ceb6fea0b45f261aa51fde941
MahiPalihawadana <dilkipalihawadana95@gmail.com>
2022-03-18 12:50:26 +0530

    cartitem modified

--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -1,8 +1,9 @@
 import './CartItem.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const CartItem = () => {
-    return  <div classname="cartitem">
+    return  (
+        <div className='cartitem'>
             <div classname="cartitem__image">
                 <img src= "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
                 alt="product name" />
@@ -12,7 +13,7 @@ const CartItem = () => {
                 <p>Product 1</p>
             </Link>
 
-            <p className='cartitem__price'>$499.99</p>
+            <p className = "cartitem__price">$499.99</p>
             
             <select className="cartitem__select" >
                 <option value="1">1</option>
@@ -21,13 +22,15 @@ const CartItem = () => {
                 <option value="4">4</option>
             </select>
 
+            
             <button className="cartitem__deleteBtn">
                 <i className="fas fa-trash"></i>
             </button>
 
 
         </div>
+    );
     
-}
+};
 
 export default CartItem;
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -4,12 +4,14 @@ import "./CartScreen.css";
 import CartItem from "../components/CartItem";
 
 const CartScreen = () => {
-    return <div className="cartscreen"> 
+    return (
+     <div className="cartscreen"> 
         <div className="cartscreen__left">
           <h2>Shopping Cart</h2>
 
           <CartItem />
         </div>
+
         <div className="cartscreen__right">
           <div className="cartscreen__info">
             <p>Subtotal (0) items</p>
@@ -22,6 +24,7 @@ const CartScreen = () => {
 
         </div>
     </div>
+    );
 };
 
 export default CartScreen;
