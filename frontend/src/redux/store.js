d47915366b5582aabe1216456fe974424c999ce9
MahiPalihawadana <dilkipalihawadana95@gmail.com>
2022-03-19 13:08:14 +0530

    redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,7 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers({});
+//Reducesrs
+import {cartReducer } from './reducers/cartReducers';
+import {getProductDetailsReducer, getProducts} from './reducers/productReducers';
+
+const reducer = combineReducers({
+    cart : cartReducer,
+});
 
 const middleware = [thunk];
 
